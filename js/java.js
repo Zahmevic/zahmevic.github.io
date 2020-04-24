9647767da804f606d402c25b73c08fae0a8100ce
zahmevic <mol18008@byui.edu>
2020-04-24 12:02:39 -0500

    Update

--- js/java.js
+++ js/java.js
@@ -1,11 +1,43 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 
-var f =new Date ();
-console.log(f.toLocaleDateString());
-var year = f.getFullYear();
-var month = f.getMonth();
-var date =f.getDate();
-var day = f.getDay();
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = month[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
 
-document.getElementById("date").textContent = f;
+document.getElementById("date").textContent = fulldate;
 // *****************************************
-
+try {
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    }
+    document.getElementById("date2").textContent = new Date().toLocaleDateString("en-US", options);
+} catch (e) {
+    alert("Error with code or your browser does not support Locale");
+}
