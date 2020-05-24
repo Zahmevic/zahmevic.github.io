639c23eea64de49abdd16083ff622325946b99fa
zahmevic <mol18008@byui.edu>
2020-05-23 20:42:45 -0500

    update

--- lesson5/js/js5.js
+++ lesson5/js/js5.js
@@ -49,4 +49,11 @@ function toggleMenu () {
 }
 
 //Friday notification
-
+let d = new Date ();
+const banner = document.getElementById('event');
+if(d.getDay == 5) {
+    banner.style.display = 'block';
+}
+else {
+    banner.style.display = 'none';
+}
