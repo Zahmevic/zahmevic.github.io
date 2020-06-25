eefa1e15a04d3622f232ca639eb3b0b7fecbe4da
zahmevic <mol18008@byui.edu>
2020-06-25 13:23:49 -0500

    update

--- lesson10/java/forecast.js
+++ lesson10/java/forecast.js
@@ -3,6 +3,7 @@ const apiURLb = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APP
 fetch(apiURLb)
 .then((response) => response.json())
 .then((jsObject) => {console.log(jsObject);
+const icon = 'http://openweathermap.org/img/wn/' + jsObject.list.filter(list.weather[0].icon + '.png');
 const week = jsObject.list.filter(h => h.dt_txt.includes('18:00:00'));
 console.log(week);
 let weekdays = ['Sun', 'Mon','Tue', 'Wen', 'Thu', 'Fri', 'Sat'];
@@ -11,6 +12,8 @@ week.forEach(forecast => {
     let d = new Date(forecast.dt_txt);
  document.getElementById(`for${dia+1}`).textContent = forecast.main.temp;
  document.getElementById(`day${dia+1}`).textContent = weekdays[d.getDay()];
+ document.getElementById(`imagesrc${dia+1}`).textContent = icon;
+ document.getElementById(`icon${dia+1}`).setAttribute('src', icon);
  dia++;
 });
 
