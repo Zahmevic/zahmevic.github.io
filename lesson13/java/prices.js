b6e357ca9777cec8360b3cf930c46f102ee82c37
zahmevic <mol18008@byui.edu>
2020-07-21 21:56:00 -0500

    upload

---
+++ lesson13/java/prices.js
@@ -0,0 +1,9 @@
+const requestURL = 'https://zahmevic.github.io/lesson13/data/prices.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); 
+  })
