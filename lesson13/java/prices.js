6c68aa9132aecec3f209c95617a39c2cfce78efe
zahmevic <mol18008@byui.edu>
2020-07-22 21:19:32 -0500

    update

--- lesson13/java/prices.js
+++ lesson13/java/prices.js
@@ -12,31 +12,39 @@ fetch(requestURL)
 let h2 = document.createElement('h2');
 let h3 = document.createElement('h3');
 let h4 = document.createElement('h4');
-let half = document.createElement('h4');
-let full = document.createElement('h4');
-let hour = document.createElement('h4');
-let Day = document.createElement('h4');
-let price1 = document.createElement('h4');
-let price2 = document.createElement('h4');
-let price3 = document.createElement('h4');
-let price4 = document.createElement('h4');
+let half = document.createElement('h5');
+let price1 = document.createElement('h6');
+let full = document.createElement('h5');
+full.setAttribute('class', full.getAttribute('class'));
+let price2 = document.createElement('h6');
+let walk = document.createElement('p');
+let hour = document.createElement('h5');
+hour.setAttribute('id', hour.getAttribute('id'));
+let price3 = document.createElement('h6');
+let Day = document.createElement('h5');
+let price4 = document.createElement('h6');
 let image = document.createElement('img');
 
 h2.textContent = table[i].name;
 h3.textContent = table[i].max;
 h4.textContent = table[i].Reservation;
 half.textContent = table[i].Half;
-full.textContent = table[i].Full;
-hour.textContent = table[i].hour;
-Day.textContent = table[i].Day;
 price1.textContent =table[i].price1;
+full.textContent = table[i].Full;
 price2.textContent =table[i].price2;
+walk.textContent = table[i].Walk;
+hour.textContent = table[i].hour;
 price3.textContent =table[i].price3;
+Day.textContent = table[i].Day;
+Day.setAttribute('id', table[i].Walk);
 price4.textContent =table[i].price4;
+image.setAttribute('src', table[i].image);
+image.setAttribute('alt', table[i].image);
 
 rentals.appendChild(h2);
 rentals.appendChild(h3);
 rentals.appendChild(h4);
+rentals.appendChild(walk);
 rentals.appendChild(half);
 rentals.appendChild(full);
 rentals.appendChild(hour);
@@ -45,6 +53,7 @@ rentals.appendChild(price1);
 rentals.appendChild(price2);
 rentals.appendChild(price3);
 rentals.appendChild(price4);
+rentals.appendChild(image);
 
 document.querySelector('div.rentals').appendChild(rentals);
     }
